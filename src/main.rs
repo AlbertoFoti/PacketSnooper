10308e64d015c52a466ca607f71c593b6fff0abd
Alberto <albertofots@gmail.com>
2022-07-18 23:31:49 +0200

    chore: pcap sample code

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ use pcap::Device;
 
 fn main() {
     println!("Hello, world!");
+
+    let devices = Device::list().unwrap();
+
+    for device in devices {
+        println!("Device name: {}", device.name);
+    }
 }
