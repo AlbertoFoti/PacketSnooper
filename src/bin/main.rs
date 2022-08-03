b734cbc5ee06b90785fecab55b5fbfe30a1001b2
Alberto <albertofots@gmail.com>
2022-08-03 14:21:19 +0200

    feat: basic app structure and packer_snooper struct implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,51 @@
-use packet_snooper::packet_snooper;
+use std::io;
+use std::io::{BufRead, Write};
+use pcap::{Device, Error};
+use packet_snooper::packet_snooper::{PacketSnooper, print_interfaces};
 
 fn main() {
-    packet_snooper::print_interfaces();
-    packet_snooper::test_simple_read_packets();
+    let mut packet_snooper = PacketSnooper::new();
+    //packet_snooper::test_simple_read_packets();
+
+    loop {
+        print_interface_menu();
+        let interface_name = get_interface_from_user().expect("Error while getting interface name from user");
+
+        match retrieve_device(interface_name.as_str()) {
+            Ok(dev) => {
+                packet_snooper.set_device(dev);
+            },
+            Err(e) => { println!("{}", e); },
+        }
+
+        println!("{}", packet_snooper);
+
+        break;
+    }
+}
+
+fn print_interface_menu() {
+    println!("----------------------------------------------------------------------------");
+    println!("---------------------------- Packet Snooper App ----------------------------");
+    print_interfaces();
+    println!("------------------------");
+    println!("Insert the interface name that you want to analyze :");
+    print!(">>> ");
+    io::stdout().flush().unwrap();
+}
+
+fn get_interface_from_user() -> Result<String, Error> {
+    let mut buffer = String::new();
+    io::stdin().lock().read_line(&mut buffer)?;
+    buffer.pop();
+    Ok(buffer)
+}
+
+fn retrieve_device(interface_name: &str) -> Result<Device, &'static str> {
+    for device in Device::list().unwrap() {
+        if interface_name == device.name {
+            return Ok(device);
+        }
+    }
+    Err("unable to find device with the specified interface name")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,63 @@ mod network_components;
 mod utility;
 
 pub mod packet_snooper {
+    use std::fmt::{Display, Formatter};
     use crate::network_components::ethernet_packet::EtherPacket;
     use pcap::{Device, Packet};
     use std::io;
     use std::io::Write;
 
+    pub struct PacketSnooper {
+        pub current_interface: Option<Device>,
+    }
+
+    impl PacketSnooper {
+        pub fn new() -> PacketSnooper {
+            PacketSnooper {
+                current_interface: None,
+            }
+        }
+
+        pub fn set_device(&mut self, device: Device) {
+            self.current_interface = Option::from(device);
+        }
+    }
+
+    impl Display for PacketSnooper {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Packet-Snooper: ").unwrap();
+            match self.current_interface.is_some() {
+                true => {
+                    write!(f, "[interface: {} / ", self.current_interface.as_ref().unwrap().name).unwrap();
+                    match self.current_interface.as_ref().unwrap().addresses.get(0).is_some() {
+                        true => {
+                            write!(f, "{:?}]", self.current_interface.as_ref().unwrap().addresses.get(0).unwrap().addr)
+                        },
+                        false => {
+                            write!(f, "None")
+                        }
+                    }
+                },
+                false => { write!(f, "[interface: None]") }
+            }
+        }
+    }
+
     pub fn print_interfaces() -> () {
+        println!("List of available interfaces: ");
         for device in Device::list().unwrap() {
-            println!("------------------------------");
-            println!("[{:?} ] : ", device.name);
+            print!("[{:?} ] : ", device.name);
+            if device.addresses.is_empty() {
+                println!();
+                break
+            };
             for address in device.addresses {
                 println!(
-                    "[{:?} / {:?}] : {:?}, {:?}",
-                    address.addr, address.netmask, address.dst_addr, address.broadcast_addr
+                    "[{:?} / {:?}]",
+                    address.addr, address.netmask
                 );
+                break;
             }
-            println!("Desc : {:?}", device.desc);
         }
     }
 
